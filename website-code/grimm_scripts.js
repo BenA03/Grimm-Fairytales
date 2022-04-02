b385443d775591e2d0ef7d3217749b2e4170153c
Ben Adams <bma54@pitt.edu>
2022-04-02 12:34:41 -0700

    created new document to manage JS

---
+++ website-code/grimm_scripts.js
@@ -0,0 +1,11 @@
+function loadDoc(num) {
+    const storylist =[ "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10", "t11", "t12", "t13", "t14"];
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", storylist[num - 1] + ".txt", true);
+    xhttp.send();
+}
